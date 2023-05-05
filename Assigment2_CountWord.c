4ee6a2c079242dfe9f92d69ac14d827a973663a2
vohieu10198 <49379002+vohieu10198@users.noreply.github.com>
2023-05-05 09:48:08 +0700

    Update Assigment2_CountWord.c

--- Assigment2_CountWord.c
+++ Assigment2_CountWord.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+char your_sentence[] = "She sells seashells on the seashore. The shells she sells are seashells";
+
+int string_length(char *str){
+    int str_length = 0; 
+    int i=0; // 
+    while(str[i] != '\0'){ 
+        ++str_length;
+        ++i;
+    }
+    return str_length;
+}
+
+void lowcase(char *str){
+     int str_length = string_length(str);
+     char *sub_str = (char*)malloc((str_length+1) * sizeof(char)); 
+    sub_str[str_length] = '\0'; 
+    for(int i=0; i <= str_length; ++i){ 
+        if(str[i] >= 'A' && str[i] <= 'Z'){ 
+            sub_str[i] = (str[i] + 32); 
+        }
+        else sub_str[i] = str[i]; 
+    }
+    printf("%s",sub_str);
+    free(sub_str);
+}
+
+int main(int argc, char const *argv[])
+{
+	  string_length(your_sentence);
+    lowcase(your_sentence);
+	
+    return 0;
+}
