7e756aef23c35b4e2e79035d8a07df7c48063df1
vohieu10198 <49379002+vohieu10198@users.noreply.github.com>
2023-07-11 15:54:10 +0700

    Delete googletest directory

--- googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/minhhieuvo/Desktop/GTest/googletest/googlemock
-# Build directory: /Users/minhhieuvo/Desktop/GTest/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
