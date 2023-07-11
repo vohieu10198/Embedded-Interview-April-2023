641805c803f5b5bd04c024b03f1263d2442320f6
vohieu10198 <49379002+vohieu10198@users.noreply.github.com>
2023-07-11 15:54:39 +0700

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/minhhieuvo/Desktop/GTest
-# Build directory: /Users/minhhieuvo/Desktop/GTest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
